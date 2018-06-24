941b7cd5f438ba0a426ee6a94e7a7aaa07c4b271
bdurb <brian@rpmgreenville.com>
2018-06-24 11:04:32 -0700

    added some default state

--- src/App.js
+++ src/App.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      list: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    };
+  };
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <h1>hello</h1>
